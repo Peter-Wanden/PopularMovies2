2dae336f4001cf107d61a63ee44e13b246ecc932
Peter.Wanden <peter.wanden@gmail.com>
2018-03-30 14:01:19 +0100

    complete ContentProvider

--- app/src/main/java/com/example/peter/popularmovies2/repository/MovieContentProvider.java
+++ app/src/main/java/com/example/peter/popularmovies2/repository/MovieContentProvider.java
@@ -1,43 +1,270 @@
 package com.example.peter.popularmovies2.repository;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.example.peter.popularmovies2.repository.MovieContract.MovieEntry;
+
+import java.util.Objects;
 
 public class MovieContentProvider extends ContentProvider {
+
+    private static final String TAG = MovieContentProvider.class.getSimpleName();
+
+    /* URI matcher for the content URI for the movies table */
+    private static final int MOVIES = 100;
+
+    /* URI matcher for the content URI for a single movie */
+    private static final int MOVIE_ID = 101;
+
+    /* UriMatcher object to match a content URI to a corresponding code. */
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        /* Code to return multiple rows */
+        sUriMatcher.addURI(
+                MovieContract.CONTENT_AUTHORITY,
+                MovieContract.PATH_MOVIE, MOVIES);
+
+        /* Code to return a single row */
+        sUriMatcher.addURI(
+                MovieContract.CONTENT_AUTHORITY,
+                MovieContract.PATH_MOVIE + "/#", MOVIE_ID);
+    }
+
+    /* Helper object */
+    private MovieDbHelper mMovieDbHelper;
+
     @Override
     public boolean onCreate() {
-        return false;
+        mMovieDbHelper = new MovieDbHelper(getContext());
+        return true;
     }
 
     @Nullable
     @Override
-    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
-        return null;
+    public Cursor query(@NonNull Uri uri,
+                        @Nullable String[] projection,
+                        @Nullable String selection,
+                        @Nullable String[] selectionArgs,
+                        @Nullable String sortOrder) {
+
+        SQLiteDatabase database = mMovieDbHelper.getReadableDatabase();
+
+        Cursor cursor;
+
+        int match = sUriMatcher.match(uri);
+
+        switch (match) {
+            case MOVIES:
+                cursor = database.query(
+                        MovieEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+
+            case MOVIE_ID:
+                selection = MOVIE_ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+
+                cursor = database.query(
+                        MovieEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+        cursor.setNotificationUri(
+                Objects.requireNonNull(
+                        getContext()).
+                        getContentResolver(),
+                uri);
+
+        return cursor;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri,
+                      @Nullable ContentValues values) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case MOVIES:
+                return insertMovie(uri, Objects.requireNonNull(values));
+            default:
+                throw new IllegalArgumentException("Insertion is not supported for " + uri);
+        }
+    }
+
+    private Uri insertMovie(Uri uri, ContentValues contentValues) {
+
+        /* Check the validity of the required values provided */
+        Integer movieId = contentValues.getAsInteger(
+                MovieEntry.COLUMN_MOVIE_ID);
+
+        if (movieId == null) {
+            throw new IllegalArgumentException("Movie requires an ID");
+        }
+
+        String movieTitle = contentValues.getAsString(
+                MovieEntry.COLUMN_TITLE);
+
+        if (movieTitle == null) {
+            throw new IllegalArgumentException("Movie requires a title");
+        }
+
+        String movieOriginalTitle = contentValues.getAsString(
+                MovieEntry.COLUMN_ORIGINAL_TITLE);
+
+        if (movieOriginalTitle == null) {
+            throw new IllegalArgumentException("Movie requires an original title");
+        }
+
+        SQLiteDatabase database = mMovieDbHelper.getWritableDatabase();
+
+        long id = database.insert(MovieEntry.TABLE_NAME, null, contentValues);
+
+        if (id == -1) {
+            Log.e(TAG, "Failed to inset row for " + uri);
+            return null;
+        }
+
+        Objects.requireNonNull(
+                getContext()).
+                getContentResolver().
+                notifyChange(uri, null);
+
+        return ContentUris.withAppendedId(uri, id);
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri,
+                      @Nullable String selection,
+                      @Nullable String[] selectionArgs) {
+
+        SQLiteDatabase database = mMovieDbHelper.getWritableDatabase();
+
+        int rowsDeleted;
+
+        final int match = sUriMatcher.match(uri);
+
+        switch (match) {
+            case MOVIES:
+                rowsDeleted = database.delete(
+                        MovieEntry.TABLE_NAME,
+                        selection,
+                        selectionArgs);
+                break;
+
+            case MOVIE_ID:
+                selection = MovieEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+
+                rowsDeleted = database.delete(
+                        MovieEntry.TABLE_NAME,
+                        selection,
+                        selectionArgs);
+                break;
+
+            default:
+                throw new IllegalArgumentException("Deletion is not supported for " + uri);
+        }
+
+        if (rowsDeleted != 0) {
+            Objects.requireNonNull(
+                    getContext()).
+                    getContentResolver().
+                    notifyChange(uri, null);
+        }
+
+        return rowsDeleted;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri,
+                      @Nullable ContentValues values,
+                      @Nullable String selection,
+                      @Nullable String[] selectionArgs) {
+        /* Check for the minimum required values */
+        assert values != null;
+        if (values.containsKey(MovieEntry.COLUMN_MOVIE_ID)) {
+            Integer movieId = values.getAsInteger(MovieEntry.COLUMN_MOVIE_ID);
+
+            if (movieId == null) {
+                throw new IllegalArgumentException("Movie requires an ID");
+            }
+        }
+
+        if (values.containsKey(MovieEntry.COLUMN_TITLE)) {
+            String movieTitle = values.getAsString(MovieEntry.COLUMN_TITLE);
+
+            if (movieTitle == null) {
+                throw new IllegalArgumentException("Movie requires a title");
+            }
+        }
+
+        if (values.containsKey(MovieEntry.COLUMN_ORIGINAL_TITLE)) {
+            String movieOriginalTitle = values.getAsString(MovieEntry.COLUMN_ORIGINAL_TITLE);
+
+            if (movieOriginalTitle == null) {
+                throw new IllegalArgumentException("Movie requires a title");
+            }
+        }
+
+        if (values.size() == 0) {
+            return 0;
+        }
+
+        SQLiteDatabase database = mMovieDbHelper.getWritableDatabase();
+
+        int rowsUpdated = database.update(
+                MovieEntry.TABLE_NAME,
+                values,
+                selection,
+                selectionArgs);
+
+        if (rowsUpdated != 0) {
+            Objects.requireNonNull(
+                    getContext()).
+                    getContentResolver().
+                    notifyChange(uri, null);
+        }
+
+        return rowsUpdated;
     }
 
     @Nullable
     @Override
     public String getType(@NonNull Uri uri) {
-        return null;
-    }
 
-    @Nullable
-    @Override
-    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
-        return null;
-    }
+        final int match = sUriMatcher.match(uri);
 
-    @Override
-    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
-        return 0;
-    }
+        switch (match) {
+            case MOVIES:
+                return MovieEntry.CONTENT_LIST_TYPE;
 
-    @Override
-    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
-        return 0;
+            case MOVIE_ID:
+                return MovieEntry.CONTENT_ITEM_TYPE;
+
+            default:
+                throw new IllegalStateException("Unknown URI " + uri + " with match " + match);
+        }
     }
 }
--- app/src/main/java/com/example/peter/popularmovies2/repository/MovieContract.java
+++ app/src/main/java/com/example/peter/popularmovies2/repository/MovieContract.java
@@ -7,8 +7,8 @@ import android.provider.BaseColumns;
 public class MovieContract {
 
     public static final String PATH_MOVIE = "movie";
-    private static final String CONTENT_AUTHORITY = "com.example.peter.popularmovies2";
-    private static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String CONTENT_AUTHORITY = "com.example.peter.popularmovies2";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
     private MovieContract() {
     }
